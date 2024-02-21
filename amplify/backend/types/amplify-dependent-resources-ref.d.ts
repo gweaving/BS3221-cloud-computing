bb11b8abe46635ef59cd3a6ce3c1ee2dd830fac7
Georgina <118439982+gweaving@users.noreply.github.com>
2024-02-21 19:03:15 +0000

    add auth

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "bs3221cloudcomputingb2c840d0": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
